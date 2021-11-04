6b96ab39d6be2f88fdd76da6006b2614c5eaf13b
sujin0529 <201802150@o.cnu.ac.kr>
2021-11-04 21:22:43 +0900

    modify Sell.sol

--- contracts/Sell.sol
+++ contracts/Sell.sol
@@ -5,19 +5,18 @@ contract Sell{
     
     struct ticketInfo{
         uint256 price;
-        address payable owner;
-        bool isSold;
+        address owner;
         bool isTransferred;
     }
     
-    //mapping(address => bytes32[]) registerTicket;
-    //mapping(address => bytes32[]) buyerTicket;
     mapping(bytes32 => ticketInfo) ticket;
     
     uint256[] theater =  [4, 5, 6];
     
     /**
     * @dev 입력받은 콘서트 정보로 hash 값을 만든다.
+    * @param _concert 공연 번호
+    * @param _number 좌석 번호
     * @return  생성된 hash 값을 반환한다.
     */
     function getHash(uint256 _concert, uint256 _number) public pure returns(bytes32) {
@@ -30,20 +29,24 @@ contract Sell{
     
     /**
     * @dev 입력받은 콘서트 정보로 hash값을 만들어 ticket 매핑에 추가한다. 
+    * @param _concert 공연 번호
+    * @param _number 좌석 수
+    * @param _vipPrice VIP 좌석의 가격
+    * @param _rPrice R 좌석의 가격
+    * @param _aPrice A 좌석의 가격
     */
     function registerConcert(uint256 _concert, uint256 _number, uint256 _vipPrice, uint256 _rPrice, uint256 _aPrice) public {
         for(uint i=0; i<_number; i++){
             bytes32 hash = getHash(_concert, i);
-            //registerTicket[msg.sender].push(hash);
             
             if(i<theater[0]){
-                ticket[hash] = ticketInfo(_vipPrice, payable(msg.sender), false, false);
+                ticket[hash] = ticketInfo(_vipPrice, msg.sender, false);
             }
             else if(i<theater[1]){
-                ticket[hash] = ticketInfo(_rPrice, payable(msg.sender), false, false);
+                ticket[hash] = ticketInfo(_rPrice, msg.sender, false);
             }
             else{
-                ticket[hash] = ticketInfo(_aPrice, payable(msg.sender), false, false);
+                ticket[hash] = ticketInfo(_aPrice, msg.sender, false);
             }
         }
         
@@ -51,34 +54,34 @@ contract Sell{
     
     /**
     * @dev 입력받은 인자에 해당하는 티켓을 구매한다.
+    * @param _concert 공연 번호
+    * @param _number 좌석 번호
     */
-    function buyTicket(uint256 _concert, uint256 _number) public payable {
+    function buyTicket(uint256 _concert, uint256 _number) public {
         bytes32 hash = getHash(_concert, _number);
-        require(ticket[hash].isSold == false, "already sold");
-        require((msg.sender).balance > ticket[hash].price, "no money");
-        ticket[hash].owner.transfer(ticket[hash].price);
-        ticket[hash].owner = payable(msg.sender);
-        ticket[hash].isSold = true;
-        //buyerTicket[msg.sender].push(hash);
+        ticket[hash].owner = msg.sender;
     }
     
     /**
     * @dev 입력받은 인자에 해당하는 티켓을 양도거래한다.
+    * @param _concert 공연 번호
+    * @param _number 좌석 수
     */
-    function transferTicket(uint256 _concert, uint256 _number) public payable{
+    function transferTicket(uint256 _concert, uint256 _number) public {
         bytes32 hash = getHash(_concert, _number);
         require(ticket[hash].isTransferred == false, "already transferred");
         ticket[hash].isTransferred = true;
-        ticket[hash].owner.transfer(ticket[hash].price);
-        ticket[hash].owner = payable(msg.sender);
-        //buyerTicket[msg.sender].push(hash);
+        ticket[hash].owner = msg.sender;
         ticket[hash].isTransferred = false;
     }
     
     /**
-    * @dev 양도 거래할 티켓의 가격을 변경한다.
+    * @dev 양도 거래할 티켓의 가격을 설정한다.
+    * @param _concert 공연 번호
+    * @param _number 좌석 번호
+    * @param _price 설정할 가격
     */
-    function changePrice(uint256 _concert, uint256 _number, uint256 _price) public {
+    function setPrice(uint256 _concert, uint256 _number, uint256 _price) public {
         bytes32 hash = getHash(_concert, _number);
         require(ticket[hash].owner == msg.sender, "not owner");
         require(ticket[hash].price >= _price, "invalid price");
@@ -87,30 +90,14 @@ contract Sell{
     
     /**
     * @dev 입력받은 티켓이 본인 것인지 확인한다.
+    * @param _concert 공연 번호
+    * @param _number 좌석 번호
     * @return  본인 것이 맞다면 true.
     */
     function checkMyTicket(uint256 _concert, uint256 _number) public view returns(bool) {
         bytes32 hash = getHash(_concert, _number);
         require(ticket[hash].owner == msg.sender, "not owner");
         return true;
-    }
-    
-    /**
-    * @dev 입력받은 콘서트의 판매 현황을 확인한다.
-    * @return  판매되었다면 false, 그렇지 않으면 true.
-    */
-    function checkMyConcert(uint256 _concert, uint256 _number) public view returns(bool[] memory) {
-        bool[] memory result = new bool[](_number);
-        for(uint i=0; i<_number; i++){
-            bytes32 hash = getHash(_concert, i);
-            if(ticket[hash].isSold){
-                result[i] = false; // you can't buy
-            }
-            else{
-                result[i] = true; // you can buy
-            }
-        }
-        return result;
     }
     
     // uint to string
